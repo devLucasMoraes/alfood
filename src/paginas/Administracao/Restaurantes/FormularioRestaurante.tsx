2c84e2612419f7dca114b0e326b66c15189d6c05
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-22 21:44:08 -0300

    axios.put

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,29 +1,46 @@
 import { Button, TextField } from "@mui/material"
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import IRestaurante from "../../../interfaces/IRestaurante";
 
 const FormularioRestaurante = () => {
 
     const [nomeRestaurante, setNomeRestaurante] = useState('')
 
+    const parametros = useParams()
+
+    useEffect(() => {
+        if (parametros.id) {
+            axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
+                //.then(reposta => console.log(reposta))
+                .then(resposta => setNomeRestaurante(resposta.data.nome))
+        }
+    }, [parametros])
+
     const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
         evento.preventDefault()
-        axios.post('http://localhost:8000/api/v2/restaurantes/',{ nome: nomeRestaurante})
-            .then(() => {
-                alert('Restaurante cadastrado')
-            })
 
-        console.log('enviar dados para api')
-        console.log(nomeRestaurante)
+        if (parametros.id) {
+            axios.put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, { nome: nomeRestaurante })
+                .then(() => {
+                    alert('Restaurante atualizado')
+                })
+        } else {
+            axios.post('http://localhost:8000/api/v2/restaurantes/', { nome: nomeRestaurante })
+                .then(() => {
+                    alert('Restaurante cadastrado')
+                })
+        }
     }
 
     return (
         <form onSubmit={aoSubmeterForm}>
-            <TextField
+            <TextField value={nomeRestaurante}
                 id="standard-basic"
                 label="Nome do restaurante"
                 variant="standard"
-                onChange={ e => setNomeRestaurante(e.target.value) }
+                onChange={e => setNomeRestaurante(e.target.value)}
             />
             <Button type="submit" variant="outlined">Salvar</Button>
         </form>
