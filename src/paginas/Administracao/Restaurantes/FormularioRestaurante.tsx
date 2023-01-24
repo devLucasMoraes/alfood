181ea06e093e39998fa978955e15cb3e8bd72f3b
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-23 22:08:46 -0300

    upgrade de layout em FormularioRestaurante

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,6 +1,6 @@
-import { Box, Button, TextField, Typography } from "@mui/material"
+import { AppBar, Box, Button, Link, Paper, TextField, Toolbar, Typography, Container } from "@mui/material"
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link as RouterLink } from "react-router-dom";
 import http from "../../../http";
 import IRestaurante from "../../../interfaces/IRestaurante";
 
@@ -35,19 +35,42 @@ const FormularioRestaurante = () => {
     }
 
     return (
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <Box component='form' onSubmit={aoSubmeterForm}>
-                <Typography component="h1" variant="h6" >Fornulário de Restaurante</Typography>
-                <TextField value={nomeRestaurante}
-                    id="standard-basic"
-                    label="Nome do restaurante"
-                    variant="standard"
-                    onChange={e => setNomeRestaurante(e.target.value)}
-                    fullWidth
-                />
-                <Button sx={{marginTop: 1}} fullWidth type="submit" variant="outlined">Salvar</Button>
+        <>
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar>
+                        <Typography variant="h6">Administração</Typography>
+                        <Box sx={{ display: 'flex', flexGrow: 1 }}>
+                            <Link component={RouterLink} to="/admin/restaurantes">
+                                <Button sx={{ my: 2, color: "white" }}>Restaurantes</Button>
+                            </Link>
+                            <Link component={RouterLink} to="/admin/restaurantes/novo">
+                                <Button sx={{ my: 2, color: "white" }}>Novo restaurante</Button>
+                            </Link>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            <Box>
+                <Container maxWidth='lg' sx={{ mt: 1 }}>
+                    <Paper sx={{ p: 2 }}>
+                        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", flexGrow: 1 }}>
+                            <Box component='form' sx={{ width: '100%'}} onSubmit={aoSubmeterForm}>
+                                <Typography component="h1" variant="h6" >Fornulário de Restaurante</Typography>
+                                <TextField value={nomeRestaurante}
+                                    id="standard-basic"
+                                    label="Nome do restaurante"
+                                    variant="standard"
+                                    onChange={e => setNomeRestaurante(e.target.value)}
+                                    fullWidth
+                                />
+                                <Button sx={{ marginTop: 1 }} fullWidth type="submit" variant="outlined">Salvar</Button>
+                            </Box>
+                        </Box>
+                    </Paper>
+                </Container>
             </Box>
-        </Box>
+        </>
     )
 }
 
